a4863e95f6bb05e4f7a6e38a5a2b2b8dc13b9c28
Xiangyu Dong <slam.printer.08@icloud.com>
2026-03-18 17:41:45 -0700

    My Agents learning note Section 2: OpenAI Agents SDK

--- 2_openai/deep_research/email_agent.py
+++ 2_openai/deep_research/email_agent.py
@@ -9,13 +9,19 @@ from agents import Agent, function_tool
 @function_tool
 def send_email(subject: str, html_body: str) -> Dict[str, str]:
     """Send an email with the given subject and HTML body"""
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
-    from_email = Email("ed@edwarddonner.com")  # put your verified sender here
-    to_email = To("ed.donner@gmail.com")  # put your recipient here
-    content = Content("text/html", html_body)
-    mail = Mail(from_email, to_email, subject, content).get()
-    response = sg.client.mail.send.post(request_body=mail)
-    print("Email response", response.status_code)
+    # sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
+    # from_email = Email("ed@edwarddonner.com")  # put your verified sender here
+    # to_email = To("ed.donner@gmail.com")  # put your recipient here
+    # content = Content("text/html", html_body)
+    # mail = Mail(from_email, to_email, subject, content).get()
+    # response = sg.client.mail.send.post(request_body=mail)
+    # print("Email response", response.status_code)
+
+    with open("lab5_email_handoff_subject.html", "w") as f:
+        f.write(subject)
+
+    with open("lab5_email_handoff_body.html", "w") as f:
+        f.write(html_body)
     return "success"
 
 
